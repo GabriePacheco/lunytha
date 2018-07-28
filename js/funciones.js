ecb778b8c949ed83057cb231fc0ad33c0391423b
Gabriel Pacheco <gabioh2012@gmail.com>
2018-07-28 00:57:33 -0500

    muchos

--- js/funciones.js
+++ js/funciones.js
@@ -9,37 +9,51 @@ var config = {
 };
 firebase.initializeApp(config);
 var  Auth = firebase.auth();
+var base = firebase.database();
 var storage = firebase.storage();
 
 
 
 Auth.onAuthStateChanged(function(user) {
   if (user) {
-  	$(".nav-content").removeClass("hide")	;
+  	$("nav").removeClass("hide")	;
   	$(".sidenav-trigger").removeClass("hide");
   	getPerfil(user);
-  	navegacion("#home");
+  	if (user.displayName != null ){
+  		navegacion("#home");
+  	}else{
+  		navegacion("#cuenta");
+  	}
   } else {
-  	$(".nav-content").addClass("hide");
+  	$("nav").addClass("hide");
   	$(".sidenav-trigger").addClass("hide");
   	navegacion("#login");
   }
 });
 
 
-
+// cerrar sesión
+$("#cerrar").click(function (){
+	firebase.auth().signOut();
+});
 
 //boton registar 
 $("#registrar").submit(function (){
-		Auth.createUserWithEmailAndPassword($("#email").val(), $("#password").val())
-	
+	if ($("#password_registro").val() == $("#password_registro2").val()){
+		firebase.auth().createUserWithEmailAndPassword($("#email_registro").val(), $("#password_registro").val())
 		.catch(function(error) {
-		  // Handle Errors here.
-		  var errorCode = error.code;
-		  var errorMessage = error.message;
-		  // ...
+		if (error.code == "auth/email-already-in-use"){
+			M.toast({html: '<span>El Email ingresado ya esta registrado <i class="material-icons red-text">error</i></span>'  });
+		}
+		
 		});
+	}else{
+		alert ("Las contraseñas no son iguales.");
+	}
+
+	return false;
 });
+
 //*** INICIA LA BARRAS
 $(document).ready(function(){
     $('.sidenav').sidenav();
@@ -49,9 +63,20 @@ $(document).ready(function(){
 
 
 $("#login").submit(function (){
+
 	Auth.signInWithEmailAndPassword($('#email_login').val(), $('#password_login').val())
 	.catch(function (error){
-		alert ("Error : " + error.message);
+		
+
+		if (error.code == "auth/user-not-found"){
+			M.toast({html: '<span>El Email ingresado no es correcto <i class="material-icons red-text">error</i></span> ' });
+		}
+
+		if (error.code == "auth/wrong-password"){
+			M.toast({html: '<span>La contraseña ingresada no es correcta <i class="material-icons red-text">error</i></span> '});
+		}
+
+
 	});
 	return false;
 });
@@ -72,12 +97,15 @@ $("a").click(function (){
 
 /**Funcion que fija la barra de navegación cuando baja el**/
 $(document).scroll(function (){
-	if ($(document).scrollTop()>50){
-		$('.nav-wrapper').hide();
+	
+	if ($(document).scrollTop() >= 50   ){
+		$("#nav").addClass("navbar-fixed");
+		$(".nav-wrapper").addClass("hide");
+		
 
 	}else{
-		$('.nav-wrapper').show();
-
+		$("#nav").removeClass("navbar-fixed");
+		$(".nav-wrapper").removeClass("hide");
 	}
 
 });
@@ -87,6 +115,7 @@ function getPerfil(usuario){
 	$("#userId").val(usuario.uid);
   	$("#email_perfil").val(usuario.email);
 	$("#nombre_perfil").val(usuario.displayName);
+
 	if (usuario.photoURL== null){
 		$("#foto").prepend("<h1><i class='large material-icons'>account_circle</i></h1>");
 	}else{
@@ -95,13 +124,21 @@ function getPerfil(usuario){
 	if (usuario.phoneNumber != null){
 		$("#telefono").val()=usuario.phoneNumber ;
 	}
+	var starCountRef = firebase.database().ref().child();
+	starCountRef.on('value', function(snapshot) {
+	  console.log(snapshot.val());
+	});
+		
 
-	
 }	
+
+//botón agregar foto de perfil
 $("#addPhotoPerfil").click(function (){
 	$("#photoURL").click();
 
 });
+
+//Escuchamos el cambio de la imagen 
 $("#photoURL").change(function (e){
 	var file = e.target.files[0];
 	if (file){
@@ -116,11 +153,9 @@ $("#photoURL").change(function (e){
 //Guardamos el perfil
 $("#perfil").submit(function(){
 	var fichero = document.getElementById("photoURL");
-
 	var imagenAsubir = fichero.files[0];
-
 	if (imagenAsubir){
-		storage.ref().child("imagenes/" + $("#userId").val()).put(imagenAsubir)
+		storage.ref().child("imagenes/userPhoto/" + $("#userId").val()).put(imagenAsubir)
 		.then(function (snap){
 				var urlImage = snap.downloadURL;
 				Auth.currentUser.updateProfile({
@@ -129,22 +164,34 @@ $("#perfil").submit(function(){
 		});
 	}
 
+	if ($("#rol").prop('checked') ) {
+		base.ref("user_config/ " + $("#userId").val()).set({
+			rol: 'representante',
+			permisos: 1,
+			mail: $("#email_perfil").val(),
+			estudiante: $("#estudiante").val()
+		});
+
+		Auth.currentUser.updateProfile({
+			phoneNumber: $("#telefono").val(),
+		
+		})
+
+
+	}
+
 	Auth.currentUser.updateProfile({
 		displayName: $("#nombre_perfil").val(),
-		
-		
-
-	}).then(function (e){
-		alert("Dotos cambiados con exito");
+	
+	})
+	.then(function (e){
+		M.toast({html: '<span>Tus datos se guardaron! <i class="material-icons green-text">done_outline</i></span>', classes: 'rounded'})
 	});
 
-	return false 
-	
-
-		
-
+	return false 	
 });
 
+//Cambiar el rol de usuario 
 $("#rol").change(function (){
 	if( $(this).prop('checked') ){
 		$(".padre").removeClass("hide");
@@ -155,4 +202,43 @@ $("#rol").change(function (){
     	$("#telefono").removeAttr("required");
 		$("#estudiante").removeAttr("required");
 	}
+});
+///////***INICIO CON FACEBOOK
+
+$("#loginConFacebook").click(function (){
+		var provider = new firebase.auth.FacebookAuthProvider();
+		firebase.auth().signInWithPopup(provider).then(function(result) {
+	  	var token = result.credential.accessToken;
+	    var user = result.user;
+	    
+	  
+	}).catch(function(error) {
+	 
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	 
+	  var email = error.email;
+	 
+	  var credential = error.credential;
+	 
+	});
+});
+
+$("#RegistrarConFacebook").click(function (){
+		var provider = new firebase.auth.FacebookAuthProvider();
+		firebase.auth().signInWithPopup(provider).then(function(result) {
+	  	var token = result.credential.accessToken;
+	    var user = result.user;
+	    
+	  
+	}).catch(function(error) {
+	 
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	 
+	  var email = error.email;
+	 
+	  var credential = error.credential;
+	 
+	});
 });
